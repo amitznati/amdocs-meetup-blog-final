d65877c96ca701da03277a4788cf183dc1481bf0
amitznati-work-hp <amitznati@gmail.com>
2021-08-18 17:26:52 +0300

    BrowserRouter to HashRouter for mobile

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   useLocation
